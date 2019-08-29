4bb66c470c59ad7859bd18b6f6d084884734982a
BreezeCat <jkjk010695@gmail.com>
2019-08-29 13:49:51 +0800

    add set_bos function some bugs exist
    Signed-off-by: BreezeCat <jkjk010695@gmail.com>

--- hybrid_A_star/hybrid_A_star.py
+++ hybrid_A_star/hybrid_A_star.py
@@ -8,8 +8,8 @@ from map_load.map_load import load_img, rgb2gray
 # Parameter
 PI = m.pi
 deltaT = 0.1 #time step
-V_set = [-1.5,1.5] # velocity sample set
-W_set = [0,PI/2,-PI/2,PI,-PI] # angular velocity sample set
+V_set = [-1.5,-1,-0.5,0.5,1,1.5] # velocity sample set
+W_set = [0,PI/2,-PI/2,PI,-PI,PI/3,-PI/3] # angular velocity sample set
 resX = 0.1 # resolution of X
 resY = 0.1 # resolution of Y
 resTH = PI/15 # resolution of theta
@@ -29,7 +29,7 @@ class node():
     def __init__(self, C, P):
         self.C = C
         self.P = P
-        self.Id = '[' + str(np.floor(self.C[0]/resX)) + ',' +  str(np.floor(self.C[1]/resY)) + ',' + str(np.floor(self.C[2]/resTH)) + ']'
+        self.Id = '[' + str(int(np.round(self.C[0]/resX))) + ',' +  str(int(np.round(self.C[1]/resY))) + ',' + str(int(np.round(self.C[2]/resTH))) + ']'
         
     def Set_cost(self, G, H):
         self.G = G
@@ -74,7 +74,7 @@ def ExpandNode(N, Ng, So, Sc):
             G, H = Cost_cal(temp, Ng, 0, 0, V, W)
             temp.Set_cost(G + N.G,20*H)
             if temp.Id in Sc:
-                pass
+                a = 1
             elif temp.Id in So:
                 if temp.G <= So[temp.Id].G:
                     So[temp.Id] = temp
@@ -127,19 +127,41 @@ def Show_path(S, Ns, Ng):
     while Nn.Id != Ns.Id:
         Path.append(Nn.C)
         Nn = S[S[Nn.Id].P]
-        plt.plot(Path[-1][0],Path[-1][1],'ro')
-        plt.arrow(Path[-1][0],Path[-1][1],0.1*m.cos(Path[-1][2]),0.1*m.sin(Path[-1][2]))
+        plt.plot(Path[-1][0]/resX,Path[-1][1]/resY,'ro')
+        plt.arrow(Path[-1][0]/resX,Path[-1][1]/resY,1*m.cos(Path[-1][2]),1*m.sin(Path[-1][2]))
+    Path.append(Nn.C)
+    plt.plot(Path[-1][0]/resX,Path[-1][1]/resY,'bo')
+    plt.arrow(Path[-1][0]/resX,Path[-1][1]/resY,1*m.cos(Path[-1][2]),1*m.sin(Path[-1][2]))
     plt.show()
     return Path
 
 
+def set_obs(Sc, im):
+    [x_size, y_size] = np.shape(im)
+    im_c = [[0 for i in range(x_size)] for j in range(y_size)]
+    for i in range(0,x_size-1):
+        for j in range(0,y_size-1):
+            im_c[x_size-i-1][j] = im[i,j]
+            if im[i,j] <= 0.9:
+                for k in range(0,int(2*PI/resTH + 1)):
+                    Id = '[' + str(j) + ',' +  str(x_size - i - 1) + ',' + str(k) + ']'                    
+                    Sc[Id] = 'obs'
+                #print(Id)
+    return Sc, im_c
+                    
+                    
+    
+
+
 if __name__ == '__main__':
-    image = rgb2gray(load_img('map_load/map/test.png'))
+    image = rgb2gray(load_img('map_load/map/test3.png'))
     a = node([5,5,PI/2],1)
     a.Set_cost(0,0)
-    b = node([8,5,PI/2],1)
+    b = node([8,5,PI],1)
     Sop = {}
     Scl = {}
+    Scl, im_c = set_obs(Scl, image)
+    plt.imshow(im_c, cmap='Greys_r', origin='lower')
     Sop, Scl, i = hybrid_A_star_process(a,b,Sop,Scl)
     Path = Show_path(Scl,a,b)
     print('Finsh',i)
--- hybrid_A_star/map_load/map_load.py
+++ hybrid_A_star/map_load/map_load.py
@@ -14,10 +14,10 @@ def load_img(name):
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
-'''
+
 if __name__ == '__main__':
     image = load_img(img_name)
     image_g = rgb2gray(image)
-    plt.imshow(image_g, cmap='Greys_r')
+    plt.imshow(image_g, cmap='Greys_r', origin='lower')
     plt.show()
-'''
+
